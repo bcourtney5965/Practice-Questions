ec06e9f9e9a18bd3a0533ffbeca6e180176963fb
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-12-05 22:00:33 -0800

    Cleaned it up a bit

--- twoBinTrees/twoBinTreesEqual.js
+++ twoBinTrees/twoBinTreesEqual.js
@@ -1,27 +1,16 @@
 let areEqual = (one, two) => {
-    // BASE CASE
-    // if they are both null
+    // BASE CASES
+    // Nodes are both null
     if (!one && !two) {
       return true;
+    // Values aren't equal
+    } else if (one === null || two === null) {
+      return false;
     } else if (one.data !== two.data) {
       return false;
-    // recursive case
+    // RECURSIVE CASE
     } else {
-      // return areEqual(one.left, one.right) && areEqual(two.left, two.right)
       return areEqual(one.left, two.left) && areEqual(one.right, two.right);
     }
     return false;
 }
-
-areEqual();
-
-
-// Bin Trees have :
-// 1.) VAL
-// 2.) Left child
-// 3.) Right child
-
-// need to:
-// 1.) verify values match
-// 2.) has same num children
-// 3.) Recurse
